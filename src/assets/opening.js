3643323dcaf88cf7fda39463b9c7fc2854ac7280
Sander Moolin <sandermoolin@gmail.com>
2018-03-15 09:10:35 -0400

    Updated welcome speech

--- src/assets/opening.js
+++ src/assets/opening.js
@@ -1,8 +1,8 @@
 export default [
-    'You were put in charge of the top floor of the library, even though there were no books there yet.',
-    'The head librarian told you not to worry. She said that the authors would be bringing them soon.',
-    'She gave you a small, frayed book and a candle. She said that you were to keep the candle lit and repeat the words in the book.',
-    '"How many times?" you asked.<br/><br/>"As many as you can," she said.',
-    'She left you upstairs behind a desk, looking out on row after row of empty shelves. There were no windows to let in the sunlight, so a dim yellow lamp sat at the end of each row.',
-    'The candle and the book sit in front of you...'
+    'You were put in charge of the top floor of the library, even though the stacks are empty.',
+    'The head librarian tells you not to worry. She says that the authors will bring the books soon.',
+    'She gives you a blank sheet of paper and a candle. She tells you to keep the candle lit and repeat the words on the paper.',
+    '"How many times?" you ask.<br/><br/>"As many as you can," she says.',
+    'She leaves you upstairs behind a desk, looking out on row after row of empty shelves. There are no windows to let in the sunlight, so a dim yellow lamp sits at the end of each row.',
+    'It is snowing outside and very cold in here. You shiver and reach out to light the candle.'
 ]
